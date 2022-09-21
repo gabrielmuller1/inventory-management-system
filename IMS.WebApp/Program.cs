011d6de85ae941a7fb5160a6d2d4ef36b3020154
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-09-21 17:41:28 -0300

    feat: productList && productListItem && searchComponent

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -3,28 +3,33 @@ using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.NovaPasta;
 using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products;
+using IMS.UseCases.Products.Interfaces;
 using IMS.WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+
 builder.Services.AddServerSideBlazor();
+
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
-builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+builder.Services.AddSingleton<IProductRepository, ProductRepository> ();
+
 builder.Services.AddScoped<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+
+builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase> ();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase> ();
 builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase> ();
+builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase> ();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
