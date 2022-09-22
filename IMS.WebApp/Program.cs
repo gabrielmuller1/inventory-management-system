b77e1480b837c237b50e0df53be1870be83b9c1f
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-09-22 11:12:36 -0300

    ✔ Add Product

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase> ();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase> ();
 builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase> ();
 builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase> ();
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase> ();
 
 var app = builder.Build();
 
