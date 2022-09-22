29d3cc87e9118fdac6e9aee67334e10e4d81b721
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-09-22 14:24:37 -0300

    ✔Add Inventory to Product

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -21,6 +21,24 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory> ();
+
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any ( x => x.Inventory != null &&
+                x.Inventory.InventoryName.Equals ( inventory.InventoryName ) ))
+            {
+                this.ProductInventories.Add ( new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = this.ProductId,
+                    Product = this
+                } );
+            }
+        }
     }
 }
 
---
+++ IMS.CoreBusiness/ProductInventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness
+{
+    public class ProductInventory
+    {
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public int InventoryId { get; set; }
+        public Inventory? Inventory { get; set; }  
+
+        public int InventoryQuantity { get; set; }
+    }
+}
