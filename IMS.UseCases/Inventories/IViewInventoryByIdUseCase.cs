3b4185aa5837e64466bae00762e04c4e2f6d86c1
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-09-26 21:46:58 -0300

    ❌ Delete IViewIventoryByIdUseCase

--- IMS.UseCases/Inventories/IViewInventoryByIdUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS.CoreBusiness;
-
-namespace IMS.UseCases.Inventories
-{
-    public interface IViewInventoryByIdUseCase
-    {
-        Task<Inventory> ExecuteAsync ( int inventoryId );
-    }
-}
